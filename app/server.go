c62ea1baa265230558b7729bfde7e28d5795aa9b
brettsmith212 <brettsmith212@gmail.com>
2024-08-02 23:17:08 -0700

    read header

--- app/server.go
+++ app/server.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
 	"strings"
 )
+
+type HTTPRequest struct {
+	Method    string
+	Path      string
+	Headers   map[string]string
+	Body      string
+	UserAgent string
+}
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -20,18 +29,63 @@ func main() {
 		os.Exit(1)
 	}
 
-	req := make([]byte, 1024)
-	conn.Read(req)
+	scanner := bufio.NewScanner(conn)
 
-	splitHeader := strings.Split(string(req), "\r\n")
-	splitRequestLine := strings.Split(splitHeader[0], " ")
+	req, err := parseStatus(scanner)
 
-	if splitRequestLine[1] == "/" {
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	} else if strings.Split(splitRequestLine[1], "/")[1] == "echo" {
-		requestBody := strings.Split(splitRequestLine[1], "/")[2]
-		conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + fmt.Sprint(len(requestBody)) + "\r\n\r\n" + requestBody))
-	} else {
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(conn, "reading standard input:", err)
 	}
+
+	var response string
+	switch path := req.Path; {
+	case strings.HasPrefix(path, "/echo/"):
+		content := strings.TrimLeft(path, "/echo/")
+		response = fmt.Sprintf(
+			"%s\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
+			getStatus(200, "OK"),
+			len(content),
+			content,
+		)
+	case path == "/user-agent":
+		response = fmt.Sprintf(
+			"%s\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
+			getStatus(200, "OK"),
+			len(req.UserAgent),
+			req.UserAgent,
+		)
+	case path == "/":
+		response = getStatus(200, "OK") + "\r\n\r\n"
+	default:
+		response = getStatus(404, "Not Found") + "\r\n\r\n"
+	}
+	conn.Write([]byte(response))
+	conn.Close()
+}
+
+func parseStatus(scanner *bufio.Scanner) (*HTTPRequest, error) {
+	var req HTTPRequest = HTTPRequest{}
+	req.Headers = make(map[string]string)
+	for i := 0; scanner.Scan(); i++ {
+		if i == 0 {
+			parts := strings.Split(scanner.Text(), " ")
+			req.Method = parts[0]
+			req.Path = parts[1]
+			continue
+		}
+		headers := strings.Split(scanner.Text(), ": ")
+		if len(headers) < 2 {
+			req.Body = headers[0]
+			break
+		}
+		if headers[0] == "User-Agent" {
+			req.UserAgent = headers[1]
+		}
+		req.Headers[headers[0]] = headers[1]
+	}
+	return &req, nil
+}
+
+func getStatus(statusCode int, statusText string) string {
+	return fmt.Sprintf("HTTP/1.1 %d %s", statusCode, statusText)
 }
