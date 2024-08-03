b5f6526e2537013edb3544267f04084be15ace20
brettsmith212 <brettsmith212@gmail.com>
2024-08-02 20:48:49 -0700

    404

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -17,6 +18,16 @@ func main() {
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+
+	req := make([]byte, 1024)
+	conn.Read(req)
+	reqStr := string(req)
+	lines := strings.Split(reqStr, "\r\n")
+	if len(lines) > 0 && lines[0] != "GET / HTTP/1.1" {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		conn.Close()
+		return
 	}
 
 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
